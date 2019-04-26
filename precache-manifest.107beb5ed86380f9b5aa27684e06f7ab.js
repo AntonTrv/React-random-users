672e4cc7c7acb26663f07c7ea9b67d4d1b17c231
AntonTrv <abtsochi@gmail.com>
2019-04-26 15:53:27 +0300

    Demo upload test

---
+++ precache-manifest.107beb5ed86380f9b5aa27684e06f7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a4854f80b31ea043ea30d34a6542d91",
+    "url": "/static/media/creative_team.9a4854f8.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "364c434c8763098761c6",
+    "url": "/static/js/main.d8976640.chunk.js"
+  },
+  {
+    "revision": "609c56dfd95f9cf6673d",
+    "url": "/static/js/2.c1ec755e.chunk.js"
+  },
+  {
+    "revision": "364c434c8763098761c6",
+    "url": "/static/css/main.cc76ac2f.chunk.css"
+  },
+  {
+    "revision": "f669970101658ca4c653d63759f03cd7",
+    "url": "/index.html"
+  }
+];
